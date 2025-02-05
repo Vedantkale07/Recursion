cd70e02634dfaa4aa77e99eb8066a04cb798fa42
vedant Kale <vedantkale0707@gmail.com>
2025-02-05 23:21:57 +0530

    add 2 program files

--- src/FibonacciNumber.java
+++ src/FibonacciNumber.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class FibonacciNumber {
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        int ans=fibo(4);
+        System.out.println(ans);
+    }
+
+    static int fibo(int n){
+
+        //base condition
+        if(n<2){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
 }
--- src/NumberExplameRecursion.java
+++ src/NumberExplameRecursion.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class NumberExplameRecursion {
+public class NumberExplameRecursion {
+    public static void main(String[] args) {
+        print1(1);
+    }
+
+    static void print1(int n){
+        if(n==5){
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        print1(n+1);
+    }
 }
